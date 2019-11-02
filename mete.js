89cc3f1fa366564c6afb0e0d48f09dc4093381b0
yasarcu <yasarcu@hotmail.com>
2019-11-02 22:50:24 +0100

    Efe ve mete sesleri eklendi, tiklaninca sayfa yenilenir

--- mete.js
+++ mete.js
@@ -1,4 +1,11 @@
-    function play(deger) {
+    function play(deger, _deger) {
     var audio=document.getElementById(deger);
+    // ikinci defer buttona tiklaninca tekrar eski rengine donsun hatasina bak
+    if(_deger.style.backgroundColor != "#e31a17") {
+    _deger.style.backgroundColor = "#e31a17";
+    }
+    else {
+    _deger.style.backgroundColor = "#000";
+    }
     audio.play();
 }
