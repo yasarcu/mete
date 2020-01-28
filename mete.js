cf93aa86bc97aa1c283d5612d4f68f62b9100351
yasarcu <yasarcu@hotmail.com>
2020-01-28 16:26:00 +0100

    her tusa tiklaninca sifir sesi geliyor

--- mete.js
+++ mete.js
@@ -20,25 +20,28 @@ $(document).ready(function () {
 });
 
 /**
- * @param {string} deger
- * @param {{ style: { backgroundColor: string; }; }} _deger
- */
+     * @param {string} deger
+     * @param {{ style: { backgroundColor: string; }; }} _deger
+     */
 function play(deger, _deger) {
-    var audio = document.getElementById(deger);
-    // var audio = $("#deger");
+    // var audio = document.getElementById(deger);
+    var audio = $("#deger");
     // ikinci defer buttona tiklaninca tekrar eski rengine donsun hatasina bak
-    var sayiBack = _deger.style.backgroundColor;
-    // var sayiBack = _deger.css('backgroundColor');
+    // var sayiBack = _deger.style.backgroundColor;
+    var sayiBack = $(_deger).css('background-color');
     if (sayiBack != "rgb(227, 26, 23)") {
-        _deger.style.backgroundColor = "#e31a17";
+        // _deger.style.backgroundColor = "#e31a17";
+        $(_deger).css("background-color" , "#e31a17");
     }
     else {
-        _deger.style.backgroundColor = "#000";
+        // _deger.style.backgroundColor = "#000";
+        $(_deger).css("background-color" , "#000");
     }
     besceTiklaAc = 3;
-    document.getElementById('kos').textContent = "Kosoferta ";
+    // document.getElementById('kos').textContent = "Kosoferta ";
+    $("#kos").text("Kosoferta ");
     // @ts-ignore
-    audio.play();
+    $("audio")[0].play();
 }
 function kosoferta() {
     besceTiklaAc--;
